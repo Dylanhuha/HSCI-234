d141e71d4c7a2c71209ad65495f426e61bcd040c
Dylanhuha <123991605+Dylanhuha@users.noreply.github.com>
2023-04-23 11:57:55 -0700

    Update main.py

--- secret/main.py
+++ secret/main.py
@@ -1,51 +1,49 @@
 import js
 p5 = js.window
-
-N = p5.random(4,8)
-n = int(N)
+program_state = 'state 1'
+image1 = p5.loadImage('image1.png')
+image2 = p5.loadImage('image2.png')
+image3 = p5.loadImage('image3.png')
 
 def setup():
-    p5.createCanvas(300, 300)    # 300 x 300 pixel canvas 
- 
-
+    p5.createCanvas(300, 300)  
+    print('finished setup..')
+    p5.imageMode(p5.CENTER)
+    
 def draw():
-    p5.background(255)           # white background
-    p5.push()  
-    p5.translate(150, p5.mouseY)
-    draw_head() 
-    draw_nose(-10,15,p5.mouseX/20)
-    draw_nose(10,15,p5.mouseX/20)
-    draw_sharkteeth(n,p5.mouseY/30) #teeth bigger with mouse y
-    draw_eye(-25, -15, 15 + p5.mouseX/20) 
-    draw_eye(25, -15, 15 + p5.mouseX/20)   
-    draw_spine(-10, 65)
-    p5.pop()  
+    p5.background(255)           
+    p5.text(str(p5.mouseX) + ", " + str(p5.mouseY), 10, 15)
+    global image1,image2,image3
+    if (program_state == "state1"):
+        p5.image(image1, 0 ,0)
+    if (millis() - program_timer > 2000):
+        program_state = "state2"
+    else if (program_state === "state2"):
+        p5.image(image2, 0 ,0)
+    else if (program_state === "state3"):
+        p5.image(image3, 0 ,0)
 
-def draw_head():
-    p5.rect(-50, -40, 100, 100)
+def keyPressed(event):
+    global program_state
+    if(program_state == "state2"):
+        program_state = "state3"
+    elif(program_state == "state3"):
+        program_state = "state2"
+    elif(program_state == "state2"):
+        program_state = "state3"
 
-def draw_nose(x, y, radius):
-    p5.push()
-    p5.noFill()
-    p5.arc(-10, 15, 20, 20, p5.radians(100), p5.radians(310))
-    p5.arc(10, 15, 20, 20, p5.radians(230), p5.radians(80))
-    p5.fill(0)
-    p5.ellipse(x, y, radius/4, radius/4)
-    p5.pop()
+def keyReleased(event):
+    print('keyReleased.. ' + str(p5.key))
+    if (key == "1"):
+        program_state = "state1"
+        program_timer = millis()
+    else if (key == "2"):
+        program_state = "state2"
+    else if (key == "3"):
+        program_state = "state3"
 
-def draw_sharkteeth(number,Size):
-    p5.push()
-    size = Size
-    p5.translate(-number*(size/2), number*5)
-    p5.fill(255)
-    for i in range(0, number): 
-        p5.triangle(0+i*size , -size, size/2+i*size, 0, size+i*size, -size)
-        p5.triangle(0+i*size , size , size/2+i*size, 0, size+i*size, size)
-    p5.pop()
+def mousePressed(event):
+    print('mousePressed..')
 
-        
-def draw_eye(x, y, radius):
-    p5.fill(255)
-    p5.ellipse(x, y, radius, radius) 
-    p5.fill(0)
-    p5.ellipse(x, y, radius/4, radius/4)
+def mouseReleased(event):
+    print('mouseReleased..')
